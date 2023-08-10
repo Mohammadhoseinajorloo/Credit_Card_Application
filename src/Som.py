a1ae73e2825ff4ccb8082666bcbea3ca3ccbf3b2
mohammad <mohammadhoseinajorloo76@gmail.com>
2023-08-10 17:07:31 +0330

    feat: install scikit-learn in som.py this feat read csv file and find X,y by vscode for work on the data this work cause we divide the data into train and test we preper the data to chenge scale by sklearn preprocessing

--- src/Som.py
+++ src/Som.py
@@ -1,6 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-dataset = pd.read_csv('data\Credit_Card_Applications.csv')
-print(dataset)
+dataset = pd.read_csv('data/Credit_Card_Applications.csv')
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:,-1].values
+
+
+print(y)
+
