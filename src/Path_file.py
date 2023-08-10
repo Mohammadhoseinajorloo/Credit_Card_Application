db48fd5892a3ecb2f5e64d12ac6dbbb39ff970a5
mohammad <mohammadhoseinajorloo76@gmail.com>
2023-08-10 22:30:49 +0430

    feat: add path file this feat add path file by atom (TE) for additon paths project and call variables into this file this work cause clean coding

---
+++ src/Path_file.py
@@ -0,0 +1,1 @@
+DATA_PATH = 'data\Credit_Card_Applications.csv'
--- src/Som.py
+++ src/Som.py
@@ -1,6 +1,36 @@
-import numpy as np
+# import librares
+import  numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('data\Credit_Card_Applications.csv')
-print(dataset)
+# import path file for read path required
+from Path_file import DATA_PATH
+
+# faind function for rean data intended for the project
+def read_data(PATH):
+    """
+    this functon basically receives an address format of data from us and
+        outputs a pandas data frame.
+            - this work by pandas library doing and uses from read_csv method
+    """
+    return pd.read_csv(PATH)
+
+
+def X_y(dataset):
+    """
+    This function it divides our data into two categoueis X, y
+        - This is done by index slacing
+    """
+    X, y = dataset.iloc[:,:-1].values, dataset.iloc[:, -1].values
+    return X, y
+
+
+def main():
+    """
+    This functon is basically our main function that executes our program
+    """
+    X, y = X_y(read_data(DATA_PATH))
+
+
+if __name__ == '__main__':
+    main()
