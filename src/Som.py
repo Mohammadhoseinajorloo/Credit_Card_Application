2466d4986bfcca3b533eb5ecf6ad4595f712e0c3
mohammad <mohammadhoseinajorloo76@gmail.com>
2023-08-12 23:34:57 +0430

    feat: overwiting This feat I rewrote the code in the form of object oriyented proggraming (OOP)

--- src/Som.py
+++ src/Som.py
@@ -1,5 +1,5 @@
 # import librares
-import  numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -28,35 +28,74 @@ def X_y(dataset):
     return X, y
 
 
-def main():
+def scaling(array, mode=None):
     """
-    This functon is basically our main function that executes our program
+
     """
-    X, y = X_y(read_data(DATA_PATH))
+    MinMax_Scaler = MinMaxScaler(feature_range=(0, 1))
+    if mode == "invers":
+        X = MinMax_Scaler.inverse_transform(array)
+    elif mode == "scaler":
+        X = MinMax_Scaler.fit_transform(array)
+    else:
+        pass
 
-    sc = MinMaxScaler(feature_range=(0, 1))
-    X = sc.fit_transform(X)
+    return X
 
+
+def _som(X):
+    """
+
+    """
     som = MiniSom(x = 10, y = 10, input_len=15, sigma=1.0, learning_rate=.5)
     som.random_weights_init(X)
     som.train_random(data=X, num_iteration=100)
+    return som
 
-    bone()
-    pcolor(som.distance_map().T)
+
+def ploting (X, y, som, show=[True, False]):
+    """
+
+    """
     marker = ['o', 's']
     colors = ['r', 'g']
+    bone()
+    pcolor(som.distance_map().T)
     for i, x in enumerate(X):
         w = som.winner(x)
         plot(w[0] + .5, w[1] + .5, marker[y[i]], markeredgecolor=colors[y[i]],
          markerfacecolor='None', markersize=10, markeredgewidth=2)
-    plt.show()
+    if show == True:
+        plt.show()
+    else:
+        pass
 
-    mappings = som.win_map(X)
-    print(mappings)
-    # frauds = mappings[(1, 1)]
-    #
-    # frauds = sc.inverse_transform(frauds)
-    # print(frauds)
+
+def mappings (X, som):
+    """
+
+    """
+    return som.win_map(X)
+
+
+def _frauds (mappings):
+    """
+
+    """
+    return mappings[(1, 1)]
+
+
+def main():
+    """
+    This functon is basically our main function that executes our program
+    """
+    X, y = X_y (read_data(DATA_PATH))
+    X = scaling (array=X, mode='scaler')
+    som = _som (X)
+    ploting (X, y, som, show=False)
+    frauds = _frauds (mappings(X, som))
+    frauds = scaling (array=frauds, mode="invers")
+    print(frauds)
 
 
 if __name__ == '__main__':
