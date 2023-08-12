df09fda6581b61e4ffdfe017c348158708291767
mohammad <mohammadhoseinajorloo76@gmail.com>
2023-08-12 14:11:19 +0430

    feat: scale, minisom, pylab This feat data scale (0, 1) by MinMaxScaler method scikit-learn and train data by minisom algorinthem neural network and visioliztion output work by pylab frauds varieble indicating persons they are victorius

--- src/Som.py
+++ src/Som.py
@@ -3,6 +3,8 @@ import  numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from minisom import MiniSom
+from pylab import bone, pcolor, colorbar, plot, show
 
 # import path file for read path required
 from Path_file import DATA_PATH
@@ -31,6 +33,30 @@ def main():
     This functon is basically our main function that executes our program
     """
     X, y = X_y(read_data(DATA_PATH))
+
+    sc = MinMaxScaler(feature_range=(0, 1))
+    X = sc.fit_transform(X)
+
+    som = MiniSom(x = 10, y = 10, input_len=15, sigma=1.0, learning_rate=.5)
+    som.random_weights_init(X)
+    som.train_random(data=X, num_iteration=100)
+
+    bone()
+    pcolor(som.distance_map().T)
+    marker = ['o', 's']
+    colors = ['r', 'g']
+    for i, x in enumerate(X):
+        w = som.winner(x)
+        plot(w[0] + .5, w[1] + .5, marker[y[i]], markeredgecolor=colors[y[i]],
+         markerfacecolor='None', markersize=10, markeredgewidth=2)
+    plt.show()
+
+    mappings = som.win_map(X)
+    print(mappings)
+    # frauds = mappings[(1, 1)]
+    #
+    # frauds = sc.inverse_transform(frauds)
+    # print(frauds)
 
 
 if __name__ == '__main__':
